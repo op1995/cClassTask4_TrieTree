07035e064292da56acdb6c77b729ddf8232e8626
op1995 <pellerofir@gmail.com>
2020-01-12 11:59:03 +0200

    fixed check for correct amount of given arguments (argc)

--- trieTree.c
+++ trieTree.c
@@ -114,7 +114,7 @@ void zeroOut(char* zeroMe, int length){
 
 int main(int argc, char const *argv[]){
 
-    if(argc==1){
+    if(argc==0){
         printf("Please use this program the intended way. Usage - give me a list of words. if the first argument is r, I will print the in reverse order.");
         exit(-1);
     }
